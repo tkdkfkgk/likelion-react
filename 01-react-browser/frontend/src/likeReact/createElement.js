dad43be4c273de51c6fbed964bd1a363f0966922
tkdkfkgk <mun961@gmail.com>
2023-02-14 02:06:51 +0900

    learn. React 처럼 작동하는 간단한 가상 DOM 구현(모방)

--- 01-react-browser/frontend/src/likeReact/createElement.js
+++ 01-react-browser/frontend/src/likeReact/createElement.js
@@ -1,11 +1,23 @@
-export const createElement = (type, props = {}, ...children) => {
+// export const createElement = (type, props = {}, ...children) => {
   
-  return {
-    '$$typeof': Symbol('virtual.dom.element'),
-    type,
-    props: {
-      ...props,
-      children
-    }
+//   return {
+//     '$$typeof': Symbol('virtual.dom.element'),
+//     type,
+//     props: {
+//       ...props,
+//       children
+//     }
+//   }
+// }
+
+export class VirtualElement {
+  $$typeof = Symbol('virtual.element');
+  constructor(type, props = {}, ...children) {
+    this.type = type;
+    this.props = { ...props, children };
   }
+}
+
+export function createElement(type, props, ...children) {
+  return new VirtualElement(type, props, ...children);
 }
