8d4291ae5bbb025a07c67098da977354c0d778f2
tkdkfkgk <mun961@gmail.com>
2023-02-14 02:13:33 +0900

    learn. React 유사한 API로 동적 마크업 생성 예시

--- 01-react-browser/frontend/src/main.js
+++ 01-react-browser/frontend/src/main.js
@@ -1,16 +1,21 @@
 import './utils/supportJavaScript.js';
+
 // import './vanilla.script.js';
-// import './react.script.js';
 
-import { createElement as h, render } from './likeReact/index.js';
+import './react.script.js';
 
-const app = h('div', {
-  className: 'app'
-}, h('h1', null, 'Hello Virtual DOM'));
+// import './likereact.script.js';
 
-render(app, document.getElementById('root'));
 
-console.log(app);
+// import { createElement as h, render } from './likeReact/index.js';
+
+// const app = h('div', {
+//   className: 'app'
+// }, h('h1', null, 'Hello Virtual DOM'));
+
+// render(app, document.getElementById('root'));
+
+// console.log(app);
 
 // 명령형 프로그래밍
 // const root = document.getElementById('root');
