0d818cca792c1551277db2a0ea88bc84286c924f
tkdkfkgk <mun961@gmail.com>
2023-02-21 16:04:21 +0900

    learn. 비동기 통신 요청/응답
    - side effect
    - componentDidMount
    
    learn. 컴포넌트 업데이트 이후 사이드 이펙트 처리 라이프 사이클 메서드
    
    - componentDidUpdate

--- 04-react-toolchain/counter-with-cra/src/components/LifeCycle/LifeCycle.jsx
+++ 04-react-toolchain/counter-with-cra/src/components/LifeCycle/LifeCycle.jsx
@@ -16,18 +16,26 @@ class LifeCycle extends React.Component {
   };
 
   render() {
-    console.log(document.querySelector('.LifeCycle')); // 안 나옵니다. - 승택 ✅
+    // console.log(document.querySelector('.LifeCycle')); // 안 나옵니다. - 승택 ✅
     return (
       <>
-        <div className="LifeCycle" tabIndex={0}>
-          컴포넌트의 생명 주기
+        <div>
+          <div className="LifeCycle" tabIndex={0}>
+            컴포넌트의 생명 주기
+          </div>
+          <input
+            id="select-me"
+            type="text"
+            placeholder="위에 요소를 클릭하면 초점이 내게 와요~"
+            aria-label="나를 선택하세요"
+          />
         </div>
-        <input
-          id="select-me"
-          type="text"
-          placeholder="위에 요소를 클릭하면 초점이 내게 와요~"
-          aria-label="나를 선택하세요"
-        />
+        <button
+          type="button"
+          onClick={() => this.setState({ message: 'updated content' })}
+        >
+          {this.state.message}
+        </button>
       </>
     );
 
@@ -47,10 +55,34 @@ class LifeCycle extends React.Component {
 
   // - 실제 DOM에 접근, 조작 (DOM 스크립트 또는 Vanilla 스크립트)
 
+  // promise (.then, .catch)
+  // async function (await, try ~ cache)
+  async fetchRandomPeople(endpoint) {
+    const response = await fetch(endpoint);
+
+    if (!response.ok) {
+      console.error(response.status, response.statusText);
+      return;
+    }
+
+    const data = await response.json();
+
+    console.log(data);
+  }
+
   // 컴포넌트가 마운트 된 이후
   // 마운트 (1회)
   componentDidMount() {
-    console.log('여기서는 실제 DOM에 접근이 가능해요');
+    // 사이드 이펙트 2번째 스토리
+    let API_ENDPOINT = 'https://randomuser.me/api/?results=5';
+
+    // API 요청/응답
+    // fetch api
+    this.fetchRandomPeople(API_ENDPOINT);
+
+    // axios library
+
+    // console.log('여기서는 실제 DOM에 접근이 가능해요');
     // console.log(document.querySelector('.LifeCycle')); // div가 나올 거 같아요. - 승택 & 선영
 
     // 바닐라 프로그래밍 (React가 아닌 것 === 사이드 이펙트)
@@ -80,6 +112,12 @@ class LifeCycle extends React.Component {
         lifecycleElement.click();
       }
     });
+  }
+
+  // 컴포넌트가 업데이트 된 이후
+  // 업데이트 (N회)
+  componentDidUpdate() {
+    console.log('우리 컴포넌트가 변경되었어요~');
   }
 }
 
