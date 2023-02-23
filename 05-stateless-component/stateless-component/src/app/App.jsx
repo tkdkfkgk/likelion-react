f77a5423b772022345d6e366d12e0183b9d75333
tkdkfkgk <mun961@gmail.com>
2023-02-23 09:17:29 +0900

    refactor. A11yHidden 코드 정리 refactor. app 디렉토리 추가 / 구성

---
+++ 05-stateless-component/stateless-component/src/app/App.jsx
@@ -0,0 +1,21 @@
+import classes from './App.module.css';
+import { Button, A11yHidden } from '@/components';
+
+function App() {
+  return (
+    <div className={classes.container}>
+      <h2 className={classes.headline}>Button 컴포넌트(stateless)</h2>
+
+      <div role="group" className={classes.buttonGroup}>
+        <Button>회원가입</Button>
+        <Button secondary>로그인</Button>
+
+        <A11yHidden as="h2">저장</A11yHidden>
+        <A11yHidden as="a">저장</A11yHidden>
+        <A11yHidden as="figcaption">저장</A11yHidden>
+      </div>
+    </div>
+  );
+}
+
+export default App;
