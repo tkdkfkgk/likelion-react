775d19bc204fc38fa77ab94e6077e24327300c25
tkdkfkgk <mun961@gmail.com>
2023-03-08 13:37:44 +0900

    learn. AtomFamily를 사용하는 이유

--- 11-recoil/learning-recoil/src/App.jsx
+++ 11-recoil/learning-recoil/src/App.jsx
@@ -1,17 +1,24 @@
 import './App.css';
+
 // import AppFooter from './AppFooter';
 // import AppHeader from './AppHeader';
 // import PlayList, { RestPlayListButton } from './PlayList';
-import ExchangePage from './ExchangePage';
+// import ExchangePage from './ExchangePage';
+import OrderListPage from './OrderListPage';
+import OrderItemPage from './OrderItemPage';
 
 function App() {
   return (
     <div className="App">
-      <ExchangePage />
-      {/* <AppHeader />
-      <PlayList />
-      <AppFooter /> 
-      <RestPlayListButton /> */}
+      <div style={{ display: 'flex', flexFlow: 'column', gap: 40 }}>
+        <OrderListPage />
+        <OrderItemPage />
+      </div>
+      {/* <ExchangePage /> */}
+      {/* <AppHeader /> */}
+      {/* <PlayList /> */}
+      {/* <AppFooter />  */}
+      {/* <RestPlayListButton /> */}
     </div>
   );
 }
